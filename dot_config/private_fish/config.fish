5781f921ec461f89ae1f21b130072057a5255d9b
Paul Cutmore <paul.cutmore@icloud.com>
2021-12-23 12:45:16 +0000

    Added aliases for exa and chezmoi to config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,6 +17,15 @@ if status is-interactive
     # Enable 'fuck' for typos
     thefuck --alias | source
 
+    # Aliases for ChezMoi
+    alias cm chezmoi
+
+    # Aliases for exa
+    alias ll "exa --long --header"
+    alias lg "exa --long --header --git"
+    alias la "exa --long --all --header"
+    alias lag "exa --long --all --header --git"
+
 # Enable autojump (j) to allow faster commonly-used folder access
     [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 

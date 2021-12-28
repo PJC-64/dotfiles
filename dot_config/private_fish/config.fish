4696e54aaa07abab3efee4effe55be84967aa9fd
PJC-64 <paul.cutmore@icloud.com>
2021-12-28 09:45:28 +0000

    Added GNU coreutils & autojump for Linux

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,18 +7,20 @@ if test "$OSTYPE" = "darwin21"
     # MacOS
     fish_add_path -pg /opt/homebrew/opt/coreutils/libexec/gnubin
     fish_add_path -pg /Users/paul/.local/bin
+    [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 else
     # Linux
     # ongoing: Add Linux/Ubuntu-specific paths
+    fish_add_path -pg /home/linuxbrew/.linuxbrew/opt/coreutils/libexec/gnubin
     fish_add_path -pg /home/linuxbrew/.linuxbrew/opt/python@3.10/bin
     fish_add_path -pg /home/.local/share/bin
     fish_add_path -pg  /home/linuxbrew/.linuxbrew/bin
+    [ -f /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish ]; and source /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish
 end
 
 # Interactive-Only
 
 if status is-interactive
-
     # Enable Glyphs
     set -g theme_powerline_fonts yes
     set -g theme_nerd_fonts yes
@@ -35,9 +37,6 @@ if status is-interactive
     alias lg "exa --long --header --git"
     alias la "exa --long --all --header"
     alias lag "exa --long --all --header --git"
-
-# Enable autojump (j) to allow faster commonly-used folder access
-    [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
-
+    cd $HOME
 end
 

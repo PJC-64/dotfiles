b34a3429b6e9bb0c8457240192e6e0474b4ed7a8
Paul Cutmore <paul.cutmore@icloud.com>
2021-12-29 14:06:43 +0000

    Updated to use different paths for linux/MacOS fzf key bindings

--- dot_config/private_fish/functions/symlink_fzf_key_bindings.fish
+++ dot_config/private_fish/functions/symlink_fzf_key_bindings.fish
@@ -1,1 +1,7 @@
-/opt/homebrew/opt/fzf/shell/key-bindings.fish
+if test "$OSTYPE" = "darwin21"
+    # MacOS
+    /opt/homebrew/opt/fzf/shell/key-bindings.fish
+else
+    # Linux
+    /home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.fish
+end

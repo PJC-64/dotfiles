d7d79f483e46d44154f36eb9f912b398291a93db
Paul Cutmore <paul.cutmore@icloud.com>
2021-12-30 17:36:16 +0000

    Fixed fzf (again!)

--- dot_config/private_fish/functions/symlink_fzf_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-if test "$OSTYPE" = "darwin21"
-    # MacOS
-    /opt/homebrew/opt/fzf/shell/key-bindings.fish
-else
-    # Linux
-    /home/linuxbrew/.linuxbrew/opt/fzf/shell/key-bindings.fish
-end

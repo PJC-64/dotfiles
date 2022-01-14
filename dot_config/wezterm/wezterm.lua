b64dedfa2b021826f7c38d0344a93be40059850c
Paul Cutmore <paul.cutmore@icloud.com>
2022-01-14 22:55:38 +0000

    Trying WezTerm with ProFontX

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require 'wezterm';
 
 return {
-  font = wezterm.font("Fira Code", {size = 16.0}),
+  font = wezterm.font_with_fallback {
+    ("ProFontX", {font-size = 14.0}),
+    ("Fira Code", {font-size = 14.0}),
 }

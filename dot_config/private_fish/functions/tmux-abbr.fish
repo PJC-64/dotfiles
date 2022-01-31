c4457a01251f3086b1f0c27f312bccd8b5260a63
Paul Cutmore <paul.cutmore@icloud.com>
2022-01-31 07:28:41 +0000

    Added tmux abbreviations, updated dot_tmux.conf.local

---
+++ dot_config/private_fish/functions/tmux-abbr.fish
@@ -0,0 +1,8 @@
+abbr --add --global ta 'tmux attach -t'
+abbr --add --global tad 'tmux attach -d -t'
+abbr --add --global ts 'tmux new-session -s'
+abbr --add --global tl 'tmux list-sessions'
+abbr --add --global tksv 'tmux kill-server'
+abbr --add --global tkss 'tmux kill-session -t'
+
+

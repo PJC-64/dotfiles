8b96f170d6e64f5d8789fb048d5c640efee08b49
Paul Cutmore <paul.cutmore@icloud.com>
2022-02-01 23:57:00 +0000

    Disowned the backgrounded dura

--- dot_config/private_fish/functions/fish_greeting.fish
+++ dot_config/private_fish/functions/fish_greeting.fish
@@ -1,6 +1,7 @@
 # Defined in /var/folders/2p/q9dqtwkj4tdckk24qvby52700000gn/T//fish.vYg7w4/fish_greeting.fish @ line 2
 function fish_greeting --description What\'s\ up,\ fish\?
     dura serve > /dev/null 2>&1 &
+    disown %1
     if test "$TERM_PROGRAM" != "tmux"; command neofetch; end
     set_color $fish_color_param
     command uname -nmsr

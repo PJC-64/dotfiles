0565895b3d2a5e48639d9371b3b73c53a58dbbde
Paul Cutmore (@Apollo) <paul.cutmore@gmail.com>
2022-11-28 18:12:32 +0000

    Fix(lvim): Updated config.lua & packer_compiled.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -66,7 +66,7 @@ lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {

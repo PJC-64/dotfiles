fe5a83d9bfda4204f816e288af3a521b90464b50
Paul Cutmore <paul.cutmore@icloud.com>
2023-03-13 20:26:51 +0000

    Fix(General): General changes including updating kitty.conf

--- dot_config/private_fish/functions/fish_greeting.fish
+++ dot_config/private_fish/functions/fish_greeting.fish
@@ -1,13 +1,13 @@
 # Defined in /var/folders/2p/q9dqtwkj4tdckk24qvby52700000gn/T//fish.vYg7w4/fish_greeting.fish @ line 2
 function fish_greeting --description What\'s\ up,\ fish\?
-    #if test "$TERM_PROGRAM" != "tmux"
-    #  command neofetch
+    if test "$TERM_PROGRAM" != "tmux"
+      command fastfetch
     #  if test -e /tmp/dura.log
     #    rm -f /tmp/dura.log
     #  end
     #  dura serve > /tmp/dura.log &
     #  disown %1
-    #end
+    end
     set_color $fish_color_param
     command uname -nmsr
     command -q -s uptime >/dev/null
